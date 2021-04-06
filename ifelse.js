9749fcdd9304c8790264e981d7a5ad1c77ebd164
sahilm-10 <82040432+sahilm-10@users.noreply.github.com>
2021-04-06 19:18:38 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,11 @@
+
+// If salary > 2000 then bonus is 500 else bonus is 200
+let salary = 3000;
+let total = salary;
+if(salary > 2000){
+    total += 500;
+}
+else{
+    total+=200;
+}
+console.log(total);
---
+++ numtype.js
@@ -0,0 +1,3 @@
+let variable = BigInt("107777777777777777777777777777777");
+console.log(typeof(variable));
+console.log(variable + BigInt("1"));
